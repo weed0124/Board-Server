698b56620ca7dc03ba71274986b3f904e514c669
weed0124 <weed0124@naver.com>
2024-01-12 15:06:25 +0900

    fetch join

--- src/main/java/practice/springmvc/domain/board/repository/jpa/SpringDataJpaBoardRepository.java
+++ src/main/java/practice/springmvc/domain/board/repository/jpa/SpringDataJpaBoardRepository.java
@@ -7,12 +7,12 @@ import practice.springmvc.domain.board.Board;
 
 import java.util.List;
 
-public interface SpringDataJpaBoardRepository extends JpaRepository<Board, Long> {
+public interface SpringDataJpaBoardRepository extends JpaRepository<Board, Long>, BoardJPARepository {
     List<Board> findByTitleLike(String title);
 
-    @Query("select b from Board b join b.member m where m.nickname like :nickname")
+    @Query("select b from Board b join fetch b.member m where m.nickname like :nickname")
     List<Board> findByNicknameLike(@Param("nickname") String nickname);
 
-    @Query("select b from Board b join b.member m where b.title like :title and m.nickname like :nickname")
+    @Query("select b from Board b join fetch b.member m where b.title like :title and m.nickname like :nickname")
     List<Board> findBoards(@Param("title") String title, @Param("nickname") String nickname);
 }
