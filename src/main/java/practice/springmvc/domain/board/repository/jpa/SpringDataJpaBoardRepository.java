ca1388631b79648e3d7b9e72135dbae1d262092c
weed0124 <weed0124@naver.com>
2024-01-12 10:49:38 +0900

    JPQL 사용으로 변경

--- src/main/java/practice/springmvc/domain/board/repository/jpa/SpringDataJpaBoardRepository.java
+++ src/main/java/practice/springmvc/domain/board/repository/jpa/SpringDataJpaBoardRepository.java
@@ -10,9 +10,9 @@ import java.util.List;
 public interface SpringDataJpaBoardRepository extends JpaRepository<Board, Long> {
     List<Board> findByTitleLike(String title);
 
-    @Query(value = "select b.* from Board b, Member m where b.member_id = m.id and m.nickname like :nickname", nativeQuery = true)
+    @Query("select b from Board b join b.member m where m.nickname like :nickname")
     List<Board> findByNicknameLike(@Param("nickname") String nickname);
 
-    @Query(value = "select b.* from Board b, Member m where b.member_id = m.id and b.title like :title and m.nickname like :nickname", nativeQuery = true)
+    @Query("select b from Board b join b.member m where b.title like :title and m.nickname like :nickname")
     List<Board> findBoards(@Param("title") String title, @Param("nickname") String nickname);
 }
