ae30fd0c5fbab7fee023dfe3ab1bd934d3bb6381
weed0124 <weed0124@naver.com>
2024-01-17 20:06:32 +0900

    Paging 시작 페이지 1부터 처리하도록 커스텀 페이징 개발

---
+++ src/main/java/practice/springmvc/domain/PageCustom.java
@@ -0,0 +1,25 @@
+package practice.springmvc.domain;
+
+import lombok.Getter;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+public class PageCustom<T> {
+    private List<T> content;
+    private PageableCustom pageableCustom;
+
+    public PageCustom(List<T> content, Pageable pageable, long total) {
+        this.content = content;
+        this.pageableCustom = new PageableCustom(new PageImpl<>(content, pageable, total));
+    }
+
+    public PageCustom(List<T> content, Pageable pageable, boolean hasNext) {
+        this.content = content;
+        this.pageableCustom = new PageableCustom(new SliceImpl<>(content, pageable, hasNext));
+    }
+}
---
+++ src/main/java/practice/springmvc/domain/PageableCustom.java
@@ -0,0 +1,37 @@
+package practice.springmvc.domain;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
+
+@Getter
+public class PageableCustom {
+    private int page;
+    private int size;
+    private boolean hasNext;
+    private boolean first;
+    private boolean last;
+    private int total;
+    private long totalPage;
+
+    public PageableCustom() {
+    }
+
+    public PageableCustom(Page page) {
+        this.page = page.getNumber() + 1;
+        this.size = page.getSize();
+        this.hasNext = page.hasNext();
+        this.first = page.isFirst();
+        this.last = page.isLast();
+        this.total = page.getTotalPages();
+        this.totalPage = page.getTotalElements();
+    }
+
+    public PageableCustom(Slice slice) {
+        this.page = slice.getNumber() + 1;
+        this.size = slice.getSize();
+        this.hasNext = slice.hasNext();
+        this.first = slice.isFirst();
+        this.last = slice.isLast();
+    }
+}
