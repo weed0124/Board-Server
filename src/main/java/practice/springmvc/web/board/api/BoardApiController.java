ef0c65dac89e69bb17877d6814e39653a118b500
weed0124 <weed0124@naver.com>
2024-02-05 16:11:38 +0900

    게시글 등록, 조회 API

--- src/main/java/practice/springmvc/web/board/api/BoardApiController.java
+++ src/main/java/practice/springmvc/web/board/api/BoardApiController.java
@@ -1,17 +1,18 @@
 package practice.springmvc.web.board.api;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import practice.springmvc.domain.board.Board;
 import practice.springmvc.domain.board.BoardSearchCond;
 import practice.springmvc.domain.board.BoardService;
+import practice.springmvc.domain.member.Member;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -42,7 +43,27 @@ public class BoardApiController {
         return ResponseEntity.ok(new Result(list));
     }
 
-    @Getter @Setter
+    @PostMapping("/add")
+    public ResponseEntity<Board> saveBoard(@RequestBody BoardWriteApiDTO boardWriteApiDTO, HttpServletRequest request) {
+        Board board = boardService.save(new Board(boardWriteApiDTO.getTitle(),
+                boardWriteApiDTO.getContent(),
+                new Member(boardWriteApiDTO.getNickname(), boardWriteApiDTO.getPassword(), boardWriteApiDTO.getIp())));
+
+        URI location = ServletUriComponentsBuilder.fromContextPath(request)
+                .path("/api/board/{id}")
+                .buildAndExpand(board.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("{id}")
+    public BoardApiDTO readBoard(@PathVariable Long id) {
+        return new BoardApiDTO(boardService.findById(id).orElseThrow());
+    }
+
+    @Getter
+    @Setter
     @AllArgsConstructor
     static class Result<T> {
         private T data;
@@ -50,6 +71,7 @@ public class BoardApiController {
 
     @Getter @Setter
     @AllArgsConstructor
+    @NoArgsConstructor
     static class BoardApiDTO {
         Long id;
         String title;
@@ -68,5 +90,17 @@ public class BoardApiController {
             this.recommendCount = board.getRecommends().size();
             this.notRecommendCount = board.getNotRecommends().size();
         }
+    }
+
+    @Getter @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class BoardWriteApiDTO {
+        Long id;
+        String title;
+        String content;
+        String nickname;
+        String password;
+        String ip;
     }
 }
