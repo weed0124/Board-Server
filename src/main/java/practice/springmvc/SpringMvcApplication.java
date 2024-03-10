e8e56c7808d091a9f416a7fa8d7696278955b680
weed0124 <weed0124@naver.com>
2024-03-10 13:55:11 +0900

    H2 -> MySQL 변경 및 Redis 추가

--- src/main/java/practice/springmvc/SpringMvcApplication.java
+++ src/main/java/practice/springmvc/SpringMvcApplication.java
@@ -2,6 +2,7 @@ package practice.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -19,6 +20,7 @@ import practice.springmvc.trace.logtrace.ThreadLocalTrace;
 //@Import(ProxyFactoryConfig.class)
 @Import(TraceAspect.class)
 @EnableJpaAuditing
+@EnableCaching
 @SpringBootApplication(scanBasePackages = {"practice.springmvc.domain", "practice.springmvc.web", "practice.springmvc.annotation", "practice.springmvc.aop"})
 public class SpringMvcApplication {
 
